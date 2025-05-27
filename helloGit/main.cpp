b3c740d524a78ef26f194d730f7e03cf8d9bacbf
Tim <tim.tamoev@gmail.com>
2025-05-27 20:39:21 +0300

    Initial Commit

---
+++ helloGit/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloGit
+//
+//  Created by Тимур Тамоев Юрьевич on 2025-05-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
